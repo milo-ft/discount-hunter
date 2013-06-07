24fb95fe2aa038b3b06b8d9a8b674a223c977d0e
mariop10 <mario.ungemach@gmail.com>
2013-06-07 10:58:54 -0400

    Initial Commit

---
+++ DiscountHunter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DiscountHunter
+//
+//  Created by iMac HS on 07-06-13.
+//  Copyright (c) 2013 Happyshop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DiscountHunter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DiscountHunter
+//
+//  Created by iMac HS on 07-06-13.
+//  Copyright (c) 2013 Happyshop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
